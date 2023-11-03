6a911d063bdfcd8592a5a156a1cdbdc357d499b3
baqir0v <tu5r38eh9@code.edu.az>
2023-11-03 13:55:22 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let discountbutton1 = document.getElementById("discountbutton1");
+let discountopen1 = document.getElementById("discountopen1");
+
+discountbutton1.addEventListener("click", () => {
+    
+    discountopen1.classList.toggle("active");
+});
+
+let discountbutton2 = document.getElementById("discountbutton2");
+let discountopen2 = document.getElementById("discountopen2");
+
+discountbutton2.addEventListener("click", () => {
+    
+    discountopen2.classList.toggle("active");
+});
